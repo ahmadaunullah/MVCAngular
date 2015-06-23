c61f439f3c37f18295a8b3f0a87d7f7939db431e
ahmadaunullah <ahmad.aunullah@yahoo.com>
2015-06-23 10:54:00 +0700

    complete

--- DBScript/inventory.sql
+++ DBScript/inventory.sql
@@ -1,1 +1,27 @@
+USE [Inventory]
 GO
+
+ Object  Table [dbo].[Customer]    Script Date 06232015 104221 
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Customer](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Address] [nvarchar](50) NULL,
+	[City] [nvarchar](50) NULL,
+	[Country] [nvarchar](50) NULL,
+	[DateOfBirth] [datetime] NULL,
+	[Age] [int] NULL,
+ CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
